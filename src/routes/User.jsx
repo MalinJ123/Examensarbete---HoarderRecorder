9edf2c1695f88013b6df711665f6a2da22b5d438
MalinJ123 <malin--jonsson@hotmail.com>
2024-02-28 14:22:50 +0100

    skeleton for user account done

--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -1,6 +1,52 @@
-
-
+import "../styles/user.css";
+import { NavLink } from "react-router-dom";
+import image from "../images/image.png";
 function User() {
-  return <h2>User account</h2>;
+  return (
+    <>
+      <div className="background">
+        <div className="user-wrapper">
+          <div className="title-logout">
+            <NavLink to="/" className="back-button">
+              <span className="material-symbols-outlined">chevron_left</span>
+            </NavLink>
+            <div></div>
+            <h1 className="title">Hej Norfe!</h1> <div></div>
+            <span className="material-symbols-outlined">logout</span>
+          </div>
+
+          <div className="label-container">
+            <label className="user-label" htmlFor="username">
+              Användarnamn
+            </label>
+            <span className="user-input">Victor</span>
+
+            {/* <input className='label'
+        type="text"
+        id="username"
+        placeholder="Victor"
+      /> */}
+          </div>
+
+          <div className="label-container">
+            <label className="user-label" htmlFor="username">
+              Kontobild
+            </label>
+            <span className="user-input">
+              example-site.com/example-avatar-image.png
+            </span>
+          </div>
+
+          <div className="image-container">
+            <img className="user-img" src={image} alt="" />
+          </div>
+          <div className="userbtn-container">
+            <button className="userbtn">Ta bort konto</button>
+            <button className="userbtn">Slutför</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default User;
