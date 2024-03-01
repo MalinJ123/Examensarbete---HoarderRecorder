f36081d61028cb95ae5bd1d137cddffd99a29845
Victor (Vigge) B. <vigge15@proton.me>
2024-03-01 14:07:52 +0100

    Added ContextRoot

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 
 import { Router } from './RouteConfig'
+import { ContextRoot } from './ContextRoot'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <ContextRoot>
+      <RouterProvider router={Router} />
+    </ContextRoot>
   </React.StrictMode>,
 )
