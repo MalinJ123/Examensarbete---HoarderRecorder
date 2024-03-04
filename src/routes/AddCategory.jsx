f988e26084763da049e068921ad4604f46333526
MalinJ123 <malin--jonsson@hotmail.com>
2024-03-04 12:02:32 +0100

    image display, title

--- src/routes/AddCategory.jsx
+++ src/routes/AddCategory.jsx
@@ -1,25 +1,39 @@
-import React, { useEffect, useContext } from "react";
-
+import React, { useEffect, useContext, useState } from "react";
 import "../styles/addcategory.css";
-
-import { AppContext } from '../ContextRoot';
+import { AppContext } from "../ContextRoot";
 
 function AddCategory() {
-
   const { setChangeButtonsOnView } = useContext(AppContext);
+  const [categoryName, setCategoryName] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
 
   useEffect(() => {
-    setChangeButtonsOnView('add-category');
-  })
+    setChangeButtonsOnView("add-category");
+  }, [setChangeButtonsOnView]);
+
+  const handleNameChange = (e) => {
+    setCategoryName(e.target.value);
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
   return (
     <>
       <div className="addcategory-container">
-        <h2 className="title">Lägg till Kategori</h2>
+        <h2 className="category-title">Lägg till Kategori</h2>
       </div>
 
       <div>
-        <div className="categorylabel-container">
+        <div className="categorylabel-container first">
           <label className="category-label" htmlFor="categoryName">
             Kategorinamn
           </label>
@@ -29,22 +43,35 @@ function AddCategory() {
             name="categoryName"
             className="category-input"
             placeholder="exempel: pokemon"
+            value={categoryName}
+            onChange={handleNameChange}
           />
         </div>
       </div>
 
       <div>
-        <div className="categorylabel-container">
-          <label className="category-label" htmlFor="imageUrl">
+        <div className="categorylabel-container second">
+          <label className="category-label" htmlFor="imageUpload">
             Lägg till en bild
           </label>
           <input
-            type="text"
-            id="imageUrl"
-            name="imageUrl"
+            type="file"
+            id="imageUpload"
+            name="imageUpload"
             className="category-input"
-            placeholder="example-site.com/example-pokemon-cards-image.png"
+            accept="image/*"
+            onChange={handleImageChange}
           />
+          {/* Visa bildförhandsgranskning om en bild har valts */}
+        </div>
+        <div className="category-image-container">
+        <div className="category-content">
+        <h2 className="category-title">Förhandsgranskning </h2>
+          {imagePreview && (
+
+            <img src={imagePreview} alt="Preview" className="image-preview" />
+          )}
+        </div>
         </div>
       </div>
     </>
--- src/routes/Authentication.jsx
+++ src/routes/Authentication.jsx
@@ -1,20 +1,21 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { AppContext } from '../ContextRoot';
+import { AppContext } from "../ContextRoot";
 
-import '../styles/authentication.css';
+import "../styles/authentication.css";
 
 export const Authentication = () => {
-    const { username, userPassword, isUserLoggedIn, setIsUserLoggedIn } = useContext(AppContext);
+  const { username, userPassword, isUserLoggedIn, setIsUserLoggedIn } =
+    useContext(AppContext);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const [selectView, setSelectView] = useState('register');
+  const [selectView, setSelectView] = useState("register");
 
-    const registerOrLoginUserGoToStart = (chosen) => {
-        if (username !== "" && userPassword !== "") {
-            /* 
+  const registerOrLoginUserGoToStart = (chosen) => {
+    if (username !== "" && userPassword !== "") {
+      /* 
                 if (chosen === 'register') {
 
                 } else {
@@ -22,138 +23,156 @@ export const Authentication = () => {
                 }
             */
 
-            setIsUserLoggedIn(true);
+      setIsUserLoggedIn(true);
 
-            navigate('/start')
-        } /* Lägg till så att man inte kan trycka på registera knappen om man inte har fyllt i fälten! */
+      navigate("/start");
+    } /* Lägg till så att man inte kan trycka på registera knappen om man inte har fyllt i fälten! */
+  };
+
+  const handleViewChange = (view) => {
+    setSelectView(view);
+  };
+
+  // If the user is already logged in, redirect to start instead of going to the login & register views
+  useEffect(() => {
+    if (isUserLoggedIn) {
+      navigate("/start");
     }
+  });
 
-    const handleViewChange = (view) => {
-        setSelectView(view);
-    };
-
-
-    // If the user is already logged in, redirect to start instead of going to the login & register views
-    useEffect(() => {
-        if (isUserLoggedIn) {
-            navigate('/start')
-        }
-    })
-
-    return (
-
-        <section className="auth__splashscreen">
-
-            {
-                selectView === 'register' ? <Register handleViewChange={handleViewChange} registerOrLoginUserGoToStart={registerOrLoginUserGoToStart} /> : <Login handleViewChange={handleViewChange} registerOrLoginUserGoToStart={registerOrLoginUserGoToStart} />
-            }
-
-        </section>
-
-    );
+  return (
+    <section className="auth__splashscreen">
+      {selectView === "register" ? (
+        <Register
+          handleViewChange={handleViewChange}
+          registerOrLoginUserGoToStart={registerOrLoginUserGoToStart}
+        />
+      ) : (
+        <Login
+          handleViewChange={handleViewChange}
+          registerOrLoginUserGoToStart={registerOrLoginUserGoToStart}
+        />
+      )}
+    </section>
+  );
 };
 
 const Register = ({ handleViewChange, registerOrLoginUserGoToStart }) => {
+  const { setUsername, setUserPassword } = useContext(AppContext);
 
-    const { setUsername, setUserPassword } = useContext(AppContext);
+  return (
+    <section className="auth__section">
+      <h1 className="auth__title">Registera nytt konto</h1>
 
-    return (
+      <div className="information__box">
+        <p>
+          <span className="hr-bold__span">Hoarder Recorder</span> är en
+          plattform där du kan sortera det du tycker är viktigt i ditt liv och
+          hålla reda på allt du håller kärt! ❤️
+        </p>
+      </div>
 
-        <section className="auth__section">
+      <form className="form__container">
+        <div className="form-input-with-label__box">
+          <label htmlFor="username__input">Användarnamn</label>
 
-            <h1 className="auth__title">Registera nytt konto</h1>
+          <input
+            type="text"
+            id="username__input"
+            placeholder="JohannaDoe"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
 
-            <div className="information__box">
+        <div className="form-input-with-label__box">
+          <label htmlFor="password__input">Lösenord</label>
 
-                <p>
-                    <span className="hr-bold__span">Hoarder Recorder</span> är en plattform där du kan sortera det du tycker är viktigt i ditt liv och
-                    hålla reda på allt du håller kärt! ❤️
-                </p>
+          <input
+            type="password"
+            id="password__input"
+            placeholder="********"
+            onChange={(e) => setUserPassword(e.target.value)}
+          />
+        </div>
 
-            </div>
+        <div className="form-button__group">
+          <button
+            type="submit"
+            className="gray__button"
+            onClick={() => registerOrLoginUserGoToStart("register")}
+          >
+            Registrera
+          </button>
 
-            <form className="form__container">
-
-                <div className="form-input-with-label__box">
-
-                    <label htmlFor="username__input">Användarnamn</label>
-
-                    <input type="text" id="username__input" placeholder="JohannaDoe" onChange={(e) => setUsername(e.target.value)} />
-
-                </div>
-
-                <div className="form-input-with-label__box">
-
-                    <label htmlFor="password__input">Lösenord</label>
-
-                    <input type="password" id="password__input" placeholder="********" onChange={(e) => setUserPassword(e.target.value)} />
-
-                </div>
-                
-                <div className="form-button__group">
-
-                    <button type="submit" className="gray__button" onClick={() => registerOrLoginUserGoToStart("register")}>Registrera</button>
-
-                    <button type="button" className="gray__button" onClick={() => handleViewChange("login")}>Gå till logga in</button>
-
-                </div>
-
-            </form>
-
-        </section>
-        
-    )
+          <button
+            type="button"
+            className="gray__button"
+            onClick={() => handleViewChange("login")}
+          >
+            Gå till logga in
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 const Login = ({ handleViewChange, registerOrLoginUserGoToStart }) => {
+  const { setUsername, setUserPassword } = useContext(AppContext);
 
-    const { setUsername, setUserPassword } = useContext(AppContext);
+  return (
+    <section className="auth__section">
+      <h1 className="auth__title">Logga in</h1>
 
-    return (
+      <div className="information__box">
+        <p>
+          <span className="hr-bold__span">Hoarder Recorder</span> är en
+          plattform där du kan sortera det du tycker är viktigt i ditt liv och
+          hålla reda på allt du håller kärt! ❤️
+        </p>
+      </div>
 
-        <section className="auth__section">
+      <form className="form__container">
+        <div className="form-input-with-label__box">
+          <label htmlFor="username__input">Användarnamn</label>
 
-            <h1 className="auth__title">Logga in</h1>
+          <input
+            type="text"
+            id="username__input"
+            placeholder="JohannaDoe"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
 
-            <div className="information__box">
+        <div className="form-input-with-label__box">
+          <label htmlFor="password__input">Lösenord</label>
 
-                <p>
-                    <span className="hr-bold__span">Hoarder Recorder</span> är en plattform där du kan sortera det du tycker är viktigt i ditt liv och
-                    hålla reda på allt du håller kärt! ❤️
-                </p>
+          <input
+            type="password"
+            id="password__input"
+            placeholder="********"
+            onChange={(e) => setUserPassword(e.target.value)}
+          />
+        </div>
 
-            </div>
+        <div className="form-button__group">
+          <button
+            type="button"
+            className="gray__button"
+            onClick={() => handleViewChange("register")}
+          >
+            Gå tillbaka till registrering
+          </button>
 
-            <form className="form__container">
-
-                <div className="form-input-with-label__box">
-
-                    <label htmlFor="username__input">Användarnamn</label>
-
-                    <input type="text" id="username__input" placeholder="JohannaDoe" onChange={(e) => setUsername(e.target.value)} />
-
-                </div>
-
-                <div className="form-input-with-label__box">
-
-                    <label htmlFor="password__input">Lösenord</label>
-
-                    <input type="password" id="password__input" placeholder="********" onChange={(e) => setUserPassword(e.target.value)} />
-
-                </div>
-
-                <div className="form-button__group">
-
-                    <button type="button" className="gray__button" onClick={() => handleViewChange("register")}>Gå tillbaka till registrering</button>
-
-                    <button type="submit" className="gray__button" onClick={() => registerOrLoginUserGoToStart("login")}>Logga in</button>
-
-                </div>
-
-
-            </form>
-
-        </section>
-
-    )
+          <button
+            type="submit"
+            className="gray__button"
+            onClick={() => registerOrLoginUserGoToStart("login")}
+          >
+            Logga in
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
