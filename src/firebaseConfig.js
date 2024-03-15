57e62f490a8057f700f6afe0bb157c72aba6d23b
Victor (Vigge) B. <vigge15@proton.me>
2024-03-15 15:17:49 +0100

    Errors

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,16 +3,14 @@ import { getFirestore } from 'firebase/firestore'
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAQCVySmh0Z8uUGYAQcc_lBXGE4vlpxKoQ",
-  authDomain: "hoarder-recorder.firebaseapp.com",
-  projectId: "hoarder-recorder",
-  storageBucket: "hoarder-recorder.appspot.com",
-  messagingSenderId: "51062678992",
-  appId: "1:51062678992:web:588e95c020bf058c8585d5"
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID
 };
 
 // Initialize Firebase
